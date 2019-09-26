6cc20052d0e987e26c3d93375164b3abde464ad0
hearsay316 <908388349@qq.com>
2019-09-26 22:07:48 +0800

    完成一个小例子

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,70 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
+      |
       <router-link to="/about">About</router-link>
     </div>
-    <div>{{takeFourNumbers}}</div>
+    <div>{{ desc }}</div>
+    <button :disabled="disAble" v-stream:click="snooze$">按钮</button>
+    <button :disabled="disAble" v-stream:click="dismiss$">停止</button>
     <router-view />
   </div>
 </template>
 <script>
-  import {interval} from 'rxjs';
-  import {  scan ,startWith ,takeWhile} from 'rxjs/operators'
-  export default{
-    name:"app",
-    subscriptions(){
-      const contdown$ = interval(1000);
-      const takeFourNumbers = contdown$.pipe(startWith(5),scan(time=>time-1),takeWhile(time=>time>0));
-      return{
-        takeFourNumbers
-      }
-    }
+import { interval, concat, of } from "rxjs";
+import {
+  map,
+  scan,
+  startWith,
+  takeWhile,
+  repeatWhen,
+  takeUntil,
+  share,
+  shareReplay
+} from "rxjs/operators";
+
+export default {
+  name: "app",
+  domStreams: ["snooze$", "dismiss$"],
+  subscriptions() {
+    const countdown$ = interval(1000);
+    const takeFourNumbers = countdown$.pipe(
+      startWith(5),
+      scan(time => time - 1),
+      takeWhile(time => time > 0)
+    );
+    let meg = concat(takeFourNumbers, of("时间到了"));
+    meg = meg.pipe(share());
+    const user = meg.pipe(
+      repeatWhen(() => this.snooze$),
+      takeUntil(this.dismiss$)
+    );
+    let desc = concat(user, of("这个是ok"));
+    desc = desc.pipe(shareReplay());
+    let disAble = desc.pipe(map(value => value !== "时间到了"));
+    return {
+      desc,
+      disAble
+    };
   }
+};
 </script>
 <style lang="stylus">
 #app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
+    font-family 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    text-align center
+    color #2c3e50
 
 #nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
+    padding 30px
+
+    a
+        font-weight bold
+        color #2c3e50
+
+        &.router-link-exact-active
+            color #42b983
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import VueRx from 'vue-rx';
+import VueRx from "vue-rx";
 Vue.config.productionTip = false;
 
 Vue.use(VueRx);
