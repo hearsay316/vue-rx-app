7cbd4a0be4f5060ae5b679151e0e540f11ecac41
hearsay316 <hearsay316@gmail.com>
2019-09-27 17:21:38 +0800

    又加了两个小例子

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
+import "buefy/dist/buefy.css";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import VueRx from "vue-rx";
+import Buefy from "buefy";
 Vue.config.productionTip = false;
 
+Vue.use(Buefy);
 Vue.use(VueRx);
 new Vue({
   router,
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,50 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <section class="section">
+      <b-tabs v-model="activeTab">
+        <b-tab-item
+          :label="person.name"
+          v-for="person of people$"
+          :key="person.id"
+        >
+        </b-tab-item>
+      </b-tabs>
+      <h1 class="title">{{ name$ }}</h1>
+      <img src="" alt="" />
+    </section>
   </div>
 </template>
+<script>
+import { of } from "rxjs";
+import { switchMap, map, catchError, pluck, share } from "rxjs/operators";
+import { ajax } from "rxjs/ajax";
+export default {
+  name: "about",
+  data() {
+    return {
+      activeTab: 0
+    };
+  },
+  domstrrams: [],
+  subscriptions() {
+    const people$ = ajax("http://localhost:3000/people").pipe(
+      map(res => res),
+      catchError(() => of("error")),
+      pluck("response")
+    );
+    const activeTab$ = this.$watchAsObservable("activeTab", {
+      immediate: true
+    }).pipe(pluck("newValue"));
+    const person$ = activeTab$.pipe(
+      switchMap(id =>
+        ajax(`http://localhost:3000/people/${id}`).pipe(
+          pluck("response"),
+          share()
+        )
+      )
+    );
+    const name$ = person$.pipe(pluck("name"));
+    return { people$, person$, name$ };
+  }
+};
+</script>
